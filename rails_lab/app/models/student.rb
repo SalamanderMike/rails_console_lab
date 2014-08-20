37cc679e24f491e31e615dd01719b635d9e40608
salamandermike <salamandermike@gmail.com>
2014-08-20 14:27:34 -0700

    fixed error on Bonus but bonus2 still not complete

--- rails_lab/app/models/student.rb
+++ rails_lab/app/models/student.rb
@@ -1,23 +1,35 @@
 class Student < ActiveRecord::Base
   validates :first_name,  :presence => true,
-                          :length => 4,
+                          :length => {:minimum => 4},
                           :uniqueness => true,
-                          :format => {:with => /\A[A-Z][a-zA-Z]+$/, :message => "Name must Capitalized and contain only letters"}
+                          :format => {:with => /\A[A-Z][a-zA-Z]+\z/, :message => "Name must Capitalized and contain only letters"}
   validates :last_name,   :presence => true,
-                          :length => 4,
+                          :length => {:minimum => 4},
                           :uniqueness => true,
-                          :format => {:with => /\A[A-Z][a-zA-Z]+$/, :message => "Name must Capitalized and contain only letters"}
-validates_with FullName, :fields => [:first_name, :last_name]
+                          :format => {:with => /\A[A-Z][a-zA-Z]+\z/, :message => "Name must Capitalized and contain only letters"}
+# validates_with FullName, :fields => [:first_name, :last_name]
 end
 
 
-# Custom Validation
-class FullName < ActiveModel::Validator
-  def validate(record)
-# rails console stopped working, so I could not finish testing/developing this idea
-    if [:fields.join].any?{|field| (field === evilNames) }
-      record.errors[:base] << "This person is evil"
-    end
-  end
-end
+# # Custom Validation
+# class FullName < ActiveModel::Validator
+#   def validate(record)
+#     names = ["TimLicata", "DelmerReed", "AnilBridgpal ", "ElieSchoppik"]
+# # rails console stopped working, so I could not finish testing/developing this idea
+#     if [:fields.join].any? |field|
+#       names.each do |name|
+#         if field === name
+#           record.send(field) == "Evil"
+#         end
+#       end
+#       record.errors[:base] << "This person is evil"
+#     end
+#   end
+# end
 
+# # Adam's Solution
+# def FullName
+#   if names.include?([first_name, last_name])
+#     errors.add(:first_name, "#{first} is evil!")
+#   end
+# end
